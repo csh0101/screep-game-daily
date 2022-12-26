a7d5f458793bf265f00e6399eca77bb428b62e82
csh0101 <1945598041@qq.com>
2022-12-26 14:38:43 +0800

    git refactor

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+import { errorMapper } from './modules/errorMapper'
+import { WORK, MOVE, CARRY } from 'screeps'
+export const loop = errorMapper(() => {
+
+    Game.resources
+    Game.creeps['Worker1'].
+})
